b096b84ef1b0e37d43172e34f635358291075137
Augustin Godiscal <dev@auggod.io>
2017-03-18 15:51:08 +0100

    zsh aliases backup

---
+++ aliases.zsh
@@ -0,0 +1,23 @@
+# GIT
+alias gfo="git fetch origin"
+alias gpo="git push origin"
+alias gr="git rebase"
+
+function wttr {
+  curl wttr.in/$1
+}
+
+# GENERAL
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+alias v="vim"
+
+# CONFIGS
+alias zshconf="open -a MacVim.app ~/.zshrc"
+alias ohmyzsh="open -a MacVim.app ~/.oh-my-zsh"
+alias vimrc="open -a MacVim.app ~/.vim/vimrc"
+alias nginxconf="open -a MacVim.app /usr/local/etc/nginx/nginx.conf"
+
+alias dev="cd ~/dev/"
